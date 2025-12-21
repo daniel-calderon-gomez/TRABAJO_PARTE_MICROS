e2f0bcaae16198f43578bb60e49815a05d1b3c0e
ArielCamposM <ariel.campos@alumnos.upm.es>
2025-12-21 17:21:31 +0100

    se crea un nuevo projecto incluyendose carpetas de Drivers y archivos .ioc y .ld

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -26,21 +26,7 @@
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-// AQUI DEBES DEFINIR TU ENUM DE ESTADOS
-typedef enum {
-    ESTADO_INICIO,
-    ESTADO_JUGADOR_VS_JUGADOR,
-    ESTADO_JUGADOR_VS_PC,
-    ESTADO_ELEGIR_SECUENCIA,
-	ESTADO_ELEGIR_SECUENCIA_PC,
-    ESTADO_ADIVINAR_SECUENCIA,
-	ESTADO_CHECK,
-    ESTADO_VICTORIA,
-	ESTADO_DERROTA
-} GameState_t;
 
-// AQUI DEBES DECLARAR LA VARIABLE GLOBAL PARA QUE OTROS ARCHIVOS C LA PUEDAN USAR
-extern GameState_t gameState;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -54,40 +40,16 @@ extern GameState_t gameState;
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-ADC_HandleTypeDef hadc1;
-
-TIM_HandleTypeDef htim3;
 
 /* USER CODE BEGIN PV */
 
-// AQUI SE DEFINE LA VARIABLE GLOBAL
-GameState_t gameState;
-// Array para guardar la secuencia secreta (5 colores: 0 a 4)
-uint8_t secret_code[5];
-// Array para guardar el intento actual del jugador (5 colores: 0 a 4)
-uint8_t current_attempt[5];
-// Índice actual de pulsación (0 a 4)
-uint8_t current_code_index = 0;
-// Contador para el Antirrebote (necesario para el callback EXTI)
-static uint32_t last_button_press_tick = 0;
-// Variables para el modo de juego (JvsJ o JvsPC) y control de turnos/intentos
-uint8_t game_mode = 0; // 0: JvsPC, 1: JvsJ
-uint8_t attempts_count = 0; // 0 a 4
-// Constantes para identificar el color registrado
-#define COLOR_ROJO      0
-#define COLOR_AMARILLO  1
-#define COLOR_VERDE     2
-#define COLOR_AZUL      3
-#define COLOR_BLANCO    4
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_ADC1_Init(void);
-static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
-void Game_FSM_Run(void);
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -124,26 +86,18 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_ADC1_Init();
-  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
-  // AQUI SE INICIALIZA LA VARIABLE
-  gameState = ESTADO_INICIO;
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-
-	  while (1)
-	      {
-
-		  Game_FSM_Run();
-
-	      }
+  while (1)
+  {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-
+  }
   /* USER CODE END 3 */
 }
 
@@ -167,7 +121,12 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 8;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -177,112 +136,15 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
   {
     Error_Handler();
   }
-}
-
-/**
-  * @brief ADC1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_ADC1_Init(void)
-{
-
-  /* USER CODE BEGIN ADC1_Init 0 */
-
-  /* USER CODE END ADC1_Init 0 */
-
-  ADC_ChannelConfTypeDef sConfig = {0};
-
-  /* USER CODE BEGIN ADC1_Init 1 */
-
-  /* USER CODE END ADC1_Init 1 */
-
-  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
-  */
-  hadc1.Instance = ADC1;
-  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
-  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = DISABLE;
-  hadc1.Init.ContinuousConvMode = DISABLE;
-  hadc1.Init.DiscontinuousConvMode = DISABLE;
-  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
-  hadc1.Init.DMAContinuousRequests = DISABLE;
-  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
-  if (HAL_ADC_Init(&hadc1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
-  */
-  sConfig.Channel = ADC_CHANNEL_0;
-  sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_28CYCLES;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN ADC1_Init 2 */
-
-  /* USER CODE END ADC1_Init 2 */
-
-}
-
-/**
-  * @brief TIM3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM3_Init(void)
-{
-
-  /* USER CODE BEGIN TIM3_Init 0 */
-
-  /* USER CODE END TIM3_Init 0 */
-
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-  /* USER CODE BEGIN TIM3_Init 1 */
-
-  /* USER CODE END TIM3_Init 1 */
-  htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 8399;
-  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 9999;
-  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM3_Init 2 */
-
-  /* USER CODE END TIM3_Init 2 */
-
 }
 
 /**
@@ -298,74 +160,17 @@ static void MX_GPIO_Init(void)
   /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOE_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_7|GPIO_PIN_8
-                          |GPIO_PIN_9, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4|GPIO_PIN_5, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : PC13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PA5 PA6 PA7 */
-  GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
+  /*Configure GPIO pin : PA5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PC4 PC5 PC7 PC8
-                           PC9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_7|GPIO_PIN_8
-                          |GPIO_PIN_9;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PB0 PB1 PB2 PB10 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_10;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PE7 */
-  GPIO_InitStruct.Pin = GPIO_PIN_7;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PB4 PB5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI1_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI2_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI2_IRQn);
-
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
@@ -374,97 +179,6 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
-extern TIM_HandleTypeDef htim3;
-
-void Game_FSM_Run(void)
-
-{
-    switch (gameState)
-    {
-        case ESTADO_INICIO:
-            // Tarea: Inicializar hardware y variables (Limpieza).
-            // Usaremos una función para esto: Game_Init_Hardware();
-            // La transición a la selección de modo se hace por el pulsador (EXTI).
-            break;
-
-        case ESTADO_JUGADOR_VS_JUGADOR:
-            // La transición a ELEGIR_SECUENCIA debe hacerse en la interrupción EXTI
-            // al pulsar el botón de confirmación en el estado ESTADO_INICIO.
-            gameState = ESTADO_ELEGIR_SECUENCIA;
-            break;
-
-        case ESTADO_JUGADOR_VS_PC:
-            // La generación de la secuencia aleatoria se hará aquí (o en ESTADO_ELEGIR_SECUENCIA_PC)
-            // game_logic_GenerarSecuenciaPC();
-            gameState = ESTADO_ELEGIR_SECUENCIA_PC;
-            break;
-
-        case ESTADO_ELEGIR_SECUENCIA_PC:
-            // ESTADO_ELEGIR_SECUENCIA_PC y ESTADO_JUGADOR_VS_PC se pueden fusionar.
-            // Si la secuencia PC se elige en el estado anterior, solo transiciona.
-            gameState = ESTADO_ADIVINAR_SECUENCIA;
-            break;
-
-        case ESTADO_ELEGIR_SECUENCIA:
-            // El núcleo de esta lógica (esperar 5 pulsaciones) se maneja por EXTI.
-            // Cuando la interrupción detecte 5 pulsaciones + botón de confirmación,
-            // la interrupción cambiará el estado a ESTADO_ADIVINAR_SECUENCIA.
-            break;
-
-        case ESTADO_ADIVINAR_SECUENCIA:
-            // El núcleo de esta lógica (esperar 5 pulsaciones) se maneja por EXTI.
-            // Cuando la interrupción detecte 5 pulsaciones + botón de confirmación:
-            // 1. La interrupción cambiará el estado a ESTADO_CHECK (que debemos crear).
-            break;
-
-        // NOTA: Creamos un nuevo estado para la evaluación
-        case ESTADO_CHECK:
-            // 1. Evaluar: game_logic_EvaluarIntento(secret_code, current_attempt);
-            // 2. Dar Feedback (LEDs)
-            // 3. Transicionar a VICTORIA, DERROTA o ESTADO_ADIVINAR_SECUENCIA
-            break;
-
-        case ESTADO_VICTORIA:
-            // Tarea: Mostrar mensaje final de victoria.
-            // La transición a ESTADO_INICIO debe hacerse al pulsar RESET (EXTI).
-            break;
-
-        case ESTADO_DERROTA:
-            // Tarea: Mostrar mensaje final de derrota.
-            // La transición a ESTADO_INICIO debe hacerse al pulsar RESET (EXTI).
-            break;
-
-        default:
-            gameState = ESTADO_INICIO;
-            break;
-    }
-}
-
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-{
-	// 1. Lógica de Antirrebote (Uso de HAL_GetTick() y temporización)
-	uint32_t current_tick = HAL_GetTick();
-
-	//(filtro de rebote: 200 ms)
-	if ((current_tick - last_button_press_tick) < 200)
-	    {
-	        return;
-	    }
-	    last_button_press_tick = current_tick;
-}
-
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-    // Verifica que la interrupción provenga de TIM3
-    if (htim->Instance == TIM3)
-    {
-        // 1. Detener el Temporizador TIM3 (CRUCIAL: para que no se repita)
-        HAL_TIM_Base_Stop_IT(&htim3);
-
-        // 2. Apagar el LED (PC4)
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4, GPIO_PIN_RESET);
-    }
-}
 /* USER CODE END 4 */
 
 /**
