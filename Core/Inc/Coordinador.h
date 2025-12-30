96296aa426fa682469013290139ace988e149872
daniel-calderon-gomez <daniel.calderong@alumnos.upm.es>
2025-12-30 14:47:39 +0100

    Se cambian estados FSM
    Se cambia algunos estados de la FSM y se empieza a crear una estructura para Coordinador_Update

--- Core/Inc/Coordinador.h
+++ Core/Inc/Coordinador.h
@@ -9,10 +9,11 @@
 #define INC_COORDINADOR_H_
 
 typedef enum {
-	INICIO, SELECCION_MODO, JUEGO1vs1, JUEGO1vsPC, VICTORIA, DERROTA
+	INICIO, SELECCION_MODO, SET_SECUENCIA, ADIVINAR, VICTORIA, DERROTA
 } FSM_JUEGO;
 
 void Coordinador_Init(void);
 void Coordinador_Update(void);
+FSM_JUEGO Coordinador_GetEstado(void);
 
 #endif /* INC_COORDINADOR_H_ */
--- Core/Src/Coordinador.c
+++ Core/Src/Coordinador.c
@@ -1,16 +1,48 @@
 //se encarga de la maquina de estados global del proyecto
 #include "Coordinador.h"
+#include "TiposJuego.h"
 
-static FSM_JUEGO estado;
+static FSM_JUEGO estado_actual;
+static ModoJuego modo_juego;
 
-void Coordinador_Init(void) {
-	estado=INICIO;
+void Coordinador_Init(void)
+{
+	estado_actual=INICIO;
+	 modo_juego = MODO_PvPC;
 }
-void Coordinador_Update(void) {
-	switch (estado){	//completar segun avancemos
+void Coordinador_Update(void)
+{
+	switch (estado_actual){
 	case INICIO:
+		 estado_actual = SELECCION_MODO;
+		 break;
+	case SELECCION_MODO:
+		//  leeremos potenciómetro + pulsador
 		break;
+
+	case SET_SECUENCIA:
+		//  PC random o jugador define secuencia
+		break;
+
+	case ADIVINAR:
+		// aquí irá la lógica del juego
+		break;
+
+	case VICTORIA:
+		// estado final
+		break;
+
+	case DERROTA:
+		// estado final
+		break;
+
 	default:
+
 		break;
 	}
+}
+
+FSM_JUEGO Coordinador_GetEstado(void)
+{
+    return estado_actual;
 }
