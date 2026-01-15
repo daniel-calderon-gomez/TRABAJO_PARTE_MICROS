63edf4f8ad586af5efaccc7f6dd6ce2f2d991a4b
ArielCamposM <ariel.campos@alumnos.upm.es>
2026-01-15 18:26:29 +0100

    Optimizacion funcion logica juego
    Se ha corregido el checkeo de la funcion que comprueba si la posicion es correcta completa o parcialmente o incorrecta. Antes podía dar un mismo color como amarillo y verde en posiciones distintas, dando lugar a un feedback incorrecto. Ahora no.

--- Core/Src/Logica_Juego.c
+++ Core/Src/Logica_Juego.c
@@ -3,27 +3,30 @@
 
 void Logica_Juego_Comparar(EventoInput objetivo[], EventoInput intento[], Resultado resultado[])
 {
-    for (int i = 0; i < MAX_PULSACIONES; i++){
-    	if (intento[i] == objetivo[i])
+	uint8_t objetivo_contado[MAX_PULSACIONES]={0};
+	uint8_t intento_contado[MAX_PULSACIONES]={0};
+
+    for (int i = 0; i < MAX_PULSACIONES; i++){	//check verde
+    	if (intento[i] == objetivo[i]){
     		resultado[i] = RES_ESTA_CORRECTO;
-
-        else
-        {
-            int encontrado = 0;
-
-            for (int j = 0; j < MAX_PULSACIONES; j++)	//corregir que un verde anterior o posterior puede dar amarillo en otra pposicion
-            {
-            	if (intento[i] == objetivo[j])
-                {
-                	encontrado = 1;
-                    break;
-                }
-            }
-
-            if (encontrado)
-                resultado[i] = RES_ESTA_OTRA_POS;
-            else
-            	resultado[i] = RES_NO_ESTA;
-        }
+    		objetivo_contado[i]=1;
+    		intento_contado[i]=1;
+    	} else resultado[i]=RES_NO_ESTA;
     }
+
+     for (int i = 0; i < MAX_PULSACIONES; i++){
+    	 if(intento_contado[i]) continue;
+
+            for (int j = 0; j < MAX_PULSACIONES; j++)
+            {
+            	if(objetivo_contado[i]) continue;
+
+            	if(intento[i]==objetivo[j]){
+            		resultado[i]=RES_ESTA_OTRA_POS;
+            		objetivo_contado[i]=1;
+            		intento_contado[i]=1;
+            		break;
+            	}
+            }
+     }
 }
